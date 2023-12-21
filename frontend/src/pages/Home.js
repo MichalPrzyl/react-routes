44df20bb6ace34f6dd33c2e59d7a0f0db8c78b22
MichalPrzyl <michal.przyl@gmail.com>
2023-12-21 21:36:58 +0100

    Add scss

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,50 @@
 const Home = () => {
-    return <h1>Home</h1>;
+  return (
+    <>
+      Witaj na moim portfolio!
+
+      Jestem doświadczonym programistą specjalizującym się w obszarze backendu. Moje umiejętności obejmują głównie język programowania Python oraz framework Django. Z pasją i zaangażowaniem rozwijam aplikacje webowe, dbając o ich efektywność, skalowalność i niezawodność.
+      Backend:
+      Python & Django
+
+      Posiadam bogate doświadczenie w tworzeniu zaawansowanych aplikacji opartych na języku Python i frameworku Django. Moje projekty obejmują różnorodne dziedziny, takie jak e-commerce, zarządzanie treścią oraz systemy informacyjne.
+      DevOps:
+      Docker & Docker Compose
+
+      Jestem specjalistą w obszarze konteneryzacji, wykorzystując technologie takie jak Docker i Docker Compose. Dzięki nim tworzę środowiska izolowane, co przekłada się na łatwość wdrażania i skalowania aplikacji.
+      Linux
+
+      Mam solidną wiedzę z zakresu systemu operacyjnego Linux, co pozwala mi efektywnie zarządzać serwerami oraz dostarczać stabilne i bezpieczne środowisko dla aplikacji.
+      CI/CD
+
+      Zajmuję się również implementacją procesów Continuous Integration (CI) i Continuous Deployment (CD), co gwarantuje automatyzację testów, budowy i wdrożeń. Dzięki temu procesy wytwarzania oprogramowania stają się bardziej efektywne i niezawodne.
+      Portfolio:
+
+      Poniżej znajdziesz kilka projektów, które reprezentują moje umiejętności:
+
+      E-commerce Platform
+      Stworzenie kompleksowej platformy e-commerce opartej na Django.
+      Integracja płatności online, zarządzanie magazynem i panel administracyjny.
+
+      System Zarządzania Treścią (CMS)
+      Rozwinięcie systemu CMS umożliwiającego łatwe dodawanie, edycję i usuwanie treści.
+      Wykorzystanie Django do stworzenia intuicyjnego interfejsu użytkownika.
+
+      Konteneryzacja i Orchestration
+      Implementacja konteneryzacji z użyciem Docker.
+      Konfiguracja Docker Compose do zarządzania wieloma kontenerami.
+
+      Automatyzacja wdrażania
+      Stworzenie procesu CI/CD do automatyzacji testów i wdrożeń.
+      Wykorzystanie narzędzi takich jak Jenkins lub GitLab CI.
+
+      Kontakt:
+
+      Chętnie podzielę się moimi doświadczeniami i pomogę Ci w zrealizowaniu projektów. Skontaktuj się ze mną poprzez formularz kontaktowy lub bezpośrednio pod adresem email@example.com.
+
+      Zapraszam do zapoznania się z moimi projektami i cieszę się na możliwość współpracy!
+    </>
+      )
   };
-  
-  export default Home;
-  
+
+export default Home;
